84029990f8e1b33678f65061f07bb2fc3879f583
bhavika267 <131829647+bhavika267@users.noreply.github.com>
2023-04-25 17:13:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const dateEl = document.getElementById('date');
+const dayEl = document.getElementById('day');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+const sessionEl = document.getElementById('session');
+const toggleBtn = document.getElementById('toggle-btn');
+let twelveHr = false;
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+const timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: twelveHr };
+setInterval(() => {
+  const date = new Date();
+  dateEl.innerText = date.toLocaleDateString('en-US', options);
+  dayEl.innerText = date.toLocaleDateString('en-US', { weekday: 'long' });
+  hoursEl.innerText = date.toLocaleTimeString('en-US', timeOptions).split(':')[0];
+  minutesEl.innerText = date.toLocaleTimeString('en-US', timeOptions).split(':')[1];
+  secondsEl.innerText = date.toLocaleTimeString('en-US', timeOptions).split(':')[2];}, 1000);
+
+toggleBtn.addEventListener('click', () => {
+  twelveHr = !twelveHr;
+  timeOptions.hour12 = twelveHr;});
+
+  /*Hi professor, I had referred github for some code references*/
